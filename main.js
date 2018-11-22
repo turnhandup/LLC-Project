722444b54f8c753325ee6e8667fe3af7a90fe5ca
unknown <panna1997@yandex.ru>
2018-11-22 21:22:02 +0200

    Animation added

---
+++ main.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    // $("#triangle-two").fadeIn(700);
+    // $("#square-two").fadeIn(1000);
+    // $("#circle-two").fadeIn(1300);
+    // $("#circle-two").delay(1500).fadeOut(700);
+    // $("#square-two").delay(2000).fadeOut(1000);
+    // $("#triangle-two").delay(2300).fadeOut(1300);
+    // $('#triangle-two').animate({
+    //     opacity: 1,
+    //     width: '+=10'
+    //   }, 5000, function() {
+    //     // Animation complete.
+    //   });
+});
